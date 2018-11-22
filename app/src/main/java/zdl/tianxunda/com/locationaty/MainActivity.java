10102e6234c3f53c7c4ef523d2a5b7c51aa1e904
zhaodongliang <1037438704@qq.com>
2018-11-22 10:15:12 +0800

    Initial commit

---
+++ app/src/main/java/zdl/tianxunda/com/locationaty/MainActivity.java
@@ -0,0 +1,53 @@
+package zdl.tianxunda.com.locationaty;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        boolean locationEnabled = isLocationEnabled();
+        if (locationEnabled == false) {
+            //进入系统设置进行打开定位权限
+            Intent intent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivityForResult(intent, 0);
+        } else {
+            Log.d("zdl", "在这里进行你要的操作");
+        }
+        Log.d("zdl", locationEnabled + "");
+
+
+    }
+
+    /**
+     * 判断定位服务是否开启
+     *
+     * @param
+     * @return true 表示开启
+     */
+    public boolean isLocationEnabled() {
+        int locationMode = 0;
+        String locationProviders;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            try {
+                locationMode = Settings.Secure.getInt(getContentResolver(), Settings.Secure.LOCATION_MODE);
+            } catch (Settings.SettingNotFoundException e) {
+                e.printStackTrace();
+                return false;
+            }
+            return locationMode != Settings.Secure.LOCATION_MODE_OFF;
+        } else {
+            locationProviders = Settings.Secure.getString(getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+            return !TextUtils.isEmpty(locationProviders);
+        }
+    }
+}
